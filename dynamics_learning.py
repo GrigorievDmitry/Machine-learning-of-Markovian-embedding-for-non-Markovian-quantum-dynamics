227f627224dc1a17edd698d14ebfc93ef610c76c
GrigorievDmitry <36789339+GrigorievDmitry@users.noreply.github.com>
2019-02-09 20:55:52 +0300

    Comments on main library (dynamics_learning)

--- dynamics_learning.py
+++ dynamics_learning.py
@@ -3,8 +3,9 @@ import numpy.random as rnd
 import scipy.linalg as la
 from scipy.stats import unitary_group as rnd_un
 
-sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]])
+sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]]) #The set of Pauli matrices
 
+#Generates two dimentional random projector
 def random_proj():
     
     x = rnd.normal(0, 1, 3)
@@ -12,6 +13,7 @@ def random_proj():
     x = x/norm
     return 0.5*(np.einsum('i,ijk->jk',x,sigma) + np.eye(2))
 
+#=================Auxilliary functions for gradient calculation===================
 def f(eig):
     eig = np.expand_dims(eig, axis = 0)
     minus = eig.T - eig
@@ -23,126 +25,67 @@ def f_hc(eig):
     minus = eig.T - eig
     plus = eig + eig.T
     return 1j*np.multiply(np.exp(1j*plus/2), np.sinc(-minus/(2*np.pi)))
-    
-#def measurement(E, env):
-#        return E.T.conj().dot(env.dot(E))
+#=================================================================================
 
+#Provides dataset generaion and Hamiltonian learning
 class dynamics_learning():
     
+    #Model initialization
     def __init__(self, sys_dim=2, mem_dim=2, res_dim=2):
-        self.sys_dim = sys_dim
-        self.mem_dim = mem_dim
-        self.res_dim = res_dim
+        self.sys_dim = sys_dim #Dimensionality of system
+        self.mem_dim = mem_dim #Dimensionality of memory
+        self.res_dim = res_dim #Dimensionality of reservoir
+        #Random Hamiltonian of the whole system
         self.h = rnd.rand(res_dim*mem_dim*sys_dim, res_dim*mem_dim*sys_dim) + 1j*rnd.rand(res_dim*mem_dim*sys_dim, res_dim*mem_dim*sys_dim)
         self.h = 0.5*self.h + 0.5*np.conj(self.h.T)
+        #Reservoir density matrix
         self.res_dens = np.eye(res_dim)
         self.res_dens[1:] = 0.
+        #Memory density matrix
         self.mem_dens = np.eye(mem_dim)
         self.mem_dens[1:] = 0.
+        #System density matrix
         self.sys_dens = np.eye(sys_dim)
         self.sys_dens[1:] = 0.
-        self.u = la.expm(-1j * self.h)
-        #resh_u = self.u.reshape(sys_dim, mem_dim, res_dim, sys_dim, mem_dim, res_dim)
-        #self.phi = np.einsum('abcdef,fh,ijclmh -> abijdelm', resh_u, self.res_dens, resh_u.conj())
-        #self.phi = self.phi.reshape(sys_dim * mem_dim, sys_dim * mem_dim, \
-                                    #sys_dim * mem_dim, sys_dim * mem_dim)
-        
+        self.u = la.expm(-1j * self.h) #Evolution operator
+    
+    #Sets predefined Hamiltonian
     def set_h(self, h):
         self.h = h
         self.u = la.expm(-1j * h)
-        #resh_u = self.u.reshape(self.sys_dim, self.mem_dim, self.res_dim, self.sys_dim, self.mem_dim, self.res_dim)
-        #self.phi = np.einsum('abcdef,fh,ijclmh->abijdelm', resh_u, self.res_dens, resh_u.conj())
-        #self.phi = self.phi.reshape(self.sys_dim * self.mem_dim, self.sys_dim * self.mem_dim, \
-                                    #self.sys_dim * self.mem_dim, self.sys_dim * self.mem_dim)
-                         
+    
+    #Sets predefined system initial state                    
     def set_in_state(self, sys_dens):
         self.sys_dens = sys_dens
-        
+    
+    #Provides state transformation with given channel    
     def phi(self, state):
         whole_state = np.kron(state, self.res_dens)
         whole_state = self.u.dot(whole_state.dot(self.u.T.conj()))
         return np.einsum('ikjk->ij', whole_state.reshape(self.sys_dim*self.mem_dim, self.res_dim, self.sys_dim*self.mem_dim, self.res_dim))
     
+    #Provides environment transformation with given channel
     def rev_phi(self, env):
         whole_env = np.kron(env, np.eye(self.res_dim))
         whole_env = self.u.T.conj().dot(whole_env.dot(self.u))
         return np.einsum('ikjl,kl->ij', whole_env.reshape(self.sys_dim*self.mem_dim, self.res_dim, self.sys_dim*self.mem_dim, self.res_dim), self.res_dens)
     
+    #Provides state or environment transformation with given projector
     @staticmethod
     def measurement(E, env):
         return E.T.conj().dot(env.dot(E))
-    
-    def get_evolution_h_pert(self, total_time_steps, h_pert):
-        state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
-        set_of_states = np.expand_dims(self.sys_dens, axis=0)
-        for i in range(1000):
-            state = self.phi(state)
-        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
-        old_h = self.h
-        self.set_h(old_h + h_pert)
-        for i in range(total_time_steps - 1):
-            state = self.phi(state)
-            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
-        self.set_h(old_h)
-            
-        return set_of_states
-    
-    def get_evolution_h_pert_continuous(self, total_time_steps, time_step, h_pert):
-        state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
-        set_of_states = np.expand_dims(self.sys_dens, axis=0)
-        for i in range(1000):
-            state = self.phi(state)
-        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
-        old_h = self.h
-        self.set_h(old_h + h_pert)
-        u = self.u
-        u = u.reshape((self.sys_dim, self.mem_dim, self.res_dim, self.sys_dim, self.mem_dim, self.res_dim))
-        rho_res = self.res_dens
-        ch = np.einsum('abcdef,fh,ijclmh->abijdelm', u, rho_res, u.conj())
-        ch = ch.reshape((self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim))
-        l = la.logm(ch)
-        ch = la.expm(time_step*l)
-        ch = ch.reshape((self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim))
-        for i in range(total_time_steps - 1):
-            state = np.einsum('ijkm,km->ij', ch, state)
-            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
-        self.set_h(old_h)
-            
-        return set_of_states
-    
-    def get_evolution_continuous(self, total_time_steps, time_step):
-        state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
-        set_of_states = np.expand_dims(self.sys_dens, axis=0)
-        for i in range(1000):
-            state = self.phi(state)
-        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
-        u = self.u
-        u = u.reshape((self.sys_dim, self.mem_dim, self.res_dim, self.sys_dim, self.mem_dim, self.res_dim))
-        rho_res = self.res_dens
-        ch = np.einsum('abcdef,fh,ijclmh->abijdelm', u, rho_res, u.conj())
-        ch = ch.reshape((self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim))
-        l = la.logm(ch)
-        ch = la.expm(time_step*l)
-        ch = ch.reshape((self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim))
-        for i in range(total_time_steps - 1):
-            state = np.einsum('ijkm,km->ij', ch, state)
-            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
-            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
-            
-        return set_of_states
-    
+
+    #Returns step-by-step evolution in time with time step equals one
     def get_evolution(self, total_time_steps):
-        state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
+        state = np.kron(self.sys_dens, self.mem_dens)
         set_of_states = np.expand_dims(self.sys_dens, axis=0)
+        #State thermalization of whole system (system + memory + reservoir)
         for i in range(1000):
             state = self.phi(state)
+        #Swapping system part with the new one
         resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
         state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
+        #Step-by-step calculation of evolution
         for i in range(total_time_steps - 1):
             state = self.phi(state)
             sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
@@ -150,6 +93,90 @@ class dynamics_learning():
             
         return set_of_states
     
+    #Returns step-by-step evolution in time with pertrubated Hamiltonian and with time step equals one
+    def get_evolution_h_pert(self, total_time_steps, h_pert):
+        state = np.kron(self.sys_dens, self.mem_dens)
+        set_of_states = np.expand_dims(self.sys_dens, axis=0)
+        #State thermalization of whole system (system + memory + reservoir)
+        for i in range(1000):
+            state = self.phi(state)
+        #Swapping system part with the new one
+        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
+        #Saving of the Hamiltonian
+        old_h = self.h
+        #Pertrubation of the Hamiltonian
+        self.set_h(old_h + h_pert)
+        #Step-by-step calculation of evolution
+        for i in range(total_time_steps - 1):
+            state = self.phi(state)
+            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
+        #Getting back to the old Hamiltonian
+        self.set_h(old_h)
+        return set_of_states
+
+    #Returns step-by-step evolution in time with pertrubated Hamiltonian and with arbitrary time step
+    def get_evolution_h_pert_continuous(self, total_time_steps, time_step, h_pert):
+        state = np.kron(self.sys_dens, self.mem_dens)
+        set_of_states = np.expand_dims(self.sys_dens, axis=0)
+        #State thermalization of whole system (system + memory + reservoir)
+        for i in range(1000):
+            state = self.phi(state)
+        #Swapping system part with the new one
+        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
+        #Saving of the Hamiltonian
+        old_h = self.h
+        #Pertrubation of the Hamiltonian
+        self.set_h(old_h + h_pert)
+        #Getting channel for the given time step
+        u = self.u
+        u = u.reshape((self.sys_dim, self.mem_dim, self.res_dim, self.sys_dim, self.mem_dim, self.res_dim))
+        rho_res = self.res_dens
+        ch = np.einsum('abcdef,fh,ijclmh->abijdelm', u, rho_res, u.conj())
+        ch = ch.reshape((self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim))
+        l = la.logm(ch)
+        ch = la.expm(time_step*l)
+        ch = ch.reshape((self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim))
+        #Step-by-step calculation of evolution
+        for i in range(total_time_steps - 1):
+            state = np.einsum('ijkm,km->ij', ch, state)
+            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
+        #Getting back to the old Hamiltonian
+        self.set_h(old_h)   
+        return set_of_states
+
+    #Returns step-by-step evolution in time without pertrubation Hamiltonian and with arbitrary time step
+    def get_evolution_continuous(self, total_time_steps, time_step):
+        state = np.kron(self.sys_dens, self.mem_dens)
+        set_of_states = np.expand_dims(self.sys_dens, axis=0)
+        #State thermalization of whole system (system + memory + reservoir)
+        for i in range(1000):
+            state = self.phi(state)
+        #Swapping system part with the new one
+        resh_state = state.reshape((self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+        state = np.kron(self.sys_dens, np.einsum('ijkj->ik', resh_state))
+        #Getting channel for the given time step
+        u = self.u
+        u = u.reshape((self.sys_dim, self.mem_dim, self.res_dim, self.sys_dim, self.mem_dim, self.res_dim))
+        rho_res = self.res_dens
+        ch = np.einsum('abcdef,fh,ijclmh->abijdelm', u, rho_res, u.conj())
+        ch = ch.reshape((self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim*self.sys_dim*self.mem_dim))
+        l = la.logm(ch)
+        ch = la.expm(time_step*l)
+        ch = ch.reshape((self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim, self.sys_dim*self.mem_dim))
+        #Step-by-step calculation of evolution
+        for i in range(total_time_steps - 1):
+            state = np.einsum('ijkm,km->ij', ch, state)
+            sys_state = np.einsum('ikjk->ij', state.reshape(self.sys_dim, self.mem_dim, self.sys_dim, self.mem_dim))
+            set_of_states = np.append(set_of_states, np.expand_dims(sys_state, axis = 0),axis = 0)
+            
+        return set_of_states
+    
+    #DEBUG
+    '''   
     def get_evolution_seq_h_pert(self, total_time_steps, h_pert_seq):
         state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
         set_of_states = np.expand_dims(self.sys_dens, axis=0)
@@ -166,10 +193,12 @@ class dynamics_learning():
             self.set_h(old_h)
             
         return set_of_states
-    
+    '''
+    #Returns training set using given Hamiltonian
     def get_tr_set(self, total_time_steps):
-        state = np.kron(self.sys_dens, self.mem_dens) #KRON != TENSOR PRODUCT!!!! RESHAPE IN MATRIX!#
+        state = np.kron(self.sys_dens, self.mem_dens)
         set_of_proj = np.empty((1, self.sys_dim * self.mem_dim, self.sys_dim * self.mem_dim), dtype = np.complex128)
+        #Step-by-step measurement simulations with evolution between them
         for i in range(total_time_steps):
             state = self.phi(state)
             proj = random_proj()
@@ -187,6 +216,7 @@ class dynamics_learning():
                 
         return set_of_proj[1:]
     
+    #Calculation of logarithmic likelihood
     def log_likelihood(self, tr_set):
         llh = 0.
         state = np.kron(self.sys_dens, self.mem_dens)
@@ -198,6 +228,8 @@ class dynamics_learning():
             llh += np.log(p)
         return llh
     
+    #DEBUG
+    '''
     def change_tr_set(self, tr_set):
         set_of_proj = np.empty((1, self.sys_dim * self.mem_dim, self.sys_dim * self.mem_dim), dtype = np.complex128)
         for e in tr_set:
@@ -208,7 +240,8 @@ class dynamics_learning():
             else:
                 set_of_proj = np.append(set_of_proj, np.expand_dims(np.kron(np.eye(2) - proj, np.eye(self.mem_dim)), axis = 0), axis = 0)
         return set_of_proj[1:]
-    
+    '''
+    #Calculation of gradient
     def grad(self, tr_set):
         val, vec = la.eig(self.h)
         num = tr_set.shape[0]
@@ -246,6 +279,7 @@ class dynamics_learning():
             
         return grd/2.
     
+    #Optimization step with Adam algorithm
     def adam_step(self, grad, learning_rate, m, v, t, b1=0.9, b2=0.999, epsilon=10.**(-8)):
         m_new = b1*m + (1 - b1)*grad
         v_new = b2*v + (1 - b2)*np.square(grad)
@@ -254,6 +288,7 @@ class dynamics_learning():
         self.set_h(self.h + learning_rate*(m_cor/(np.sqrt(v_cor + epsilon))))
         return m_new, v_new
     
+    #Optimization step with momentum algorithm
     def momentum_step(self, grad, learning_rate, m, gamma=0.9):
         m_new = gamma*m + (1 - gamma)*grad
         self.set_h(self.h + learning_rate*m_new)
