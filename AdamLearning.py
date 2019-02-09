6ef38d30d177506053927942f15f9e920442aba7
GrigorievDmitry <36789339+GrigorievDmitry@users.noreply.github.com>
2019-02-09 21:23:11 +0300

    Comments on execution file (AdamLearning)

--- AdamLearning.py
+++ AdamLearning.py
@@ -5,56 +5,76 @@ import dynamics_learning as dl
 from dynamics_learning import dynamics_learning as dl1
 from scipy.optimize import fmin_l_bfgs_b as bfgs
 
-sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]])
+sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]]) #The set of Pauli matrices
 
 
-
+#Initialization of two models (model_1 for dataset generation and model_2 for learning)
 model_1 = dl.dynamics_learning(2, 2, 2)
 model_2 = dl.dynamics_learning(2, 2, 4)
 model_1.set_in_state(np.array([[0., 0.], [0., 1.]]))
 model_2.set_in_state(np.array([[0., 0.], [0., 1.]]))
 
+#Hamiltonian of model_2
 h = rnd.rand(4,4) + 1j*rnd.rand(4,4)
 h = np.kron(h + h.conj().T, np.eye(4))
-#h_true = rnd.rand(4,4) + 1j*rnd.rand(4,4)
-#h_true = h_true + h_true.conj().T
-#h_true = np.kron(h_true + h_true.conj().T, np.eye(2))
-#h_true = 0.8*h_true
-#h_s = rnd.rand(2,2) + 1j*rnd.rand(2,2)
-#h_s = h_s + h_s.T.conj()
-#h_m = rnd.rand(2,2) + 1j*rnd.rand(2,2)
-#h_m = h_s + h_s.T.conj()
-#h_r = rnd.rand(2,2) + 1j*rnd.rand(2,2)
-#h_r = h_s + h_s.T.conj()
-#h_ism = rnd.rand(4,4) + 1j*rnd.rand(4,4)
-#h_ism = h_ism + h_ism.T.conj()
-#h_imr = rnd.rand(4,4) + 1j*rnd.rand(4,4)
-#h_imr = h_imr + h_imr.T.conj()
-#h_isr = rnd.rand(4,4) + 1j*rnd.rand(4,4)
-#h_isr = h_isr + h_isr.T.conj()
-#h_isr = np.kron(h_isr, np.eye(2)).reshape((2,2,2,2,2,2))
-#h_isr = h_isr.transpose((0,2,1,3,5,4))
-#h_isr = h_isr.reshape((8,8))
-#h_true = 0.6*np.kron(h_s, np.eye(4)) + 0.6*np.kron(np.eye(4), h_r)\
-# + 0.6*np.kron(np.kron(np.eye(2), h_m), np.eye(2)) + 0.15*np.kron(np.eye(2), h_imr)\
-# + 0.3*np.kron(h_ism, np.eye(2)) + 0.1*h_isr
-h_true = np.load('h_true.npy')
-np.save('h_true.npy', h_true)
+
+#Hamiltonian of model_1
+h_true = rnd.rand(4,4) + 1j*rnd.rand(4,4)
+h_true = h_true + h_true.conj().T
+h_true = np.kron(h_true + h_true.conj().T, np.eye(2))
+h_true = 0.8*h_true
+h_s = rnd.rand(2,2) + 1j*rnd.rand(2,2)
+h_s = h_s + h_s.T.conj()
+h_m = rnd.rand(2,2) + 1j*rnd.rand(2,2)
+h_m = h_s + h_s.T.conj()
+h_r = rnd.rand(2,2) + 1j*rnd.rand(2,2)
+h_r = h_s + h_s.T.conj()
+h_ism = rnd.rand(4,4) + 1j*rnd.rand(4,4)
+h_ism = h_ism + h_ism.T.conj()
+h_imr = rnd.rand(4,4) + 1j*rnd.rand(4,4)
+h_imr = h_imr + h_imr.T.conj()
+h_isr = rnd.rand(4,4) + 1j*rnd.rand(4,4)
+h_isr = h_isr + h_isr.T.conj()
+h_isr = np.kron(h_isr, np.eye(2)).reshape((2,2,2,2,2,2))
+h_isr = h_isr.transpose((0,2,1,3,5,4))
+h_isr = h_isr.reshape((8,8))
+h_true = 0.6*np.kron(h_s, np.eye(4)) + 0.6*np.kron(np.eye(4), h_r)\
+ + 0.6*np.kron(np.kron(np.eye(2), h_m), np.eye(2)) + 0.15*np.kron(np.eye(2), h_imr)\
+ + 0.3*np.kron(h_ism, np.eye(2)) + 0.1*h_isr
+np.save('h_true.npy', h_true) #Saving of model_1 Hamiltonian
+
+#==================================PARAMETERS===================================
+batch_size = 10**3
+number_of_time_steps = 10**5
+learning_rate = 0.001
+number_of_epochs = 300
+plotting_time_step = 0.1
+max_total_plotting_time = 200
+min_total_plotting_time = 20
+#===============================================================================
+
+max_time_steps = int(max_total_plotting_time/plotting_time_step)
+min_time_steps = int(min_total_plotting_time/plotting_time_step)
+
+#Hamiltonian setting
 model_1.set_h(h_true)
 model_2.set_h(h)
-tr_set = model_1.get_tr_set(100000)
-batch_size = 1000
-tr_set_reshaped = tr_set.reshape((-1, batch_size, tr_set.shape[1], tr_set.shape[2]))
-p1 = model_1.log_likelihood(tr_set)
+
+#Training set generation
+tr_set = model_1.get_tr_set(number_of_time_steps)
+
+tr_set_reshaped = tr_set.reshape((-1, batch_size, tr_set.shape[1], tr_set.shape[2])) #Splitting of training set
+p1 = model_1.log_likelihood(tr_set) #True loglikelihood
+
+#=========================LEARNING CICLE AND PLOTTING===========================
 m = 0.
 v = 0.
 t = 1.
-learning_rate = 0.001
 counter = 0
 llh = np.array([])
-true_llh = np.full(300, p1)
+true_llh = np.full(number_of_epochs, p1)
 
-for epoch in range(300):
+for epoch in range(number_of_epochs):
     llh_agr = 0.
     for batch in range(tr_set_reshaped.shape[0]):
         grad = model_2.grad(tr_set_reshaped[batch])
@@ -72,13 +92,13 @@ for epoch in range(300):
     plt.xlabel('Epochs')
     plt.savefig(f'pic/likelihood_fig{counter}.pdf')
     plt.pause(0.05)
-    T = np.arange(0, 200, 0.1)
-    rho1 = model_1.get_evolution_continuous(2000, 0.1)
-    rho2 = model_2.get_evolution_continuous(2000, 0.1)
+    T = np.arange(0, max_total_plotting_time, plotting_time_step)
+    rho1 = model_1.get_evolution_continuous(max_time_steps, plotting_time_step)
+    rho2 = model_2.get_evolution_continuous(max_time_steps, plotting_time_step)
     model_1.set_in_state(np.array([[1., 0.], [0., 1.]])/2)
     model_2.set_in_state(np.array([[1., 0.], [0., 1.]])/2)
-    rho_zero1 = model_1.get_evolution_continuous(2000, 0.1)
-    rho_zero2 = model_2.get_evolution_continuous(2000, 0.1)
+    rho_zero1 = model_1.get_evolution_continuous(max_time_steps, plotting_time_step)
+    rho_zero2 = model_2.get_evolution_continuous(max_time_steps, plotting_time_step)
     model_1.set_in_state(np.array([[0., 0.], [0., 1.]]))
     model_2.set_in_state(np.array([[0., 0.], [0., 1.]]))
     sx1 = np.einsum('kij,ij->k', rho1, sigma[0])
@@ -127,27 +147,27 @@ for epoch in range(300):
     plt.subplot(2,2,1)
     plt.ylim(top = 1)
     plt.ylim(bottom = -1)
-    plt.plot(T[:200], sx1[:200], 'r')
-    plt.plot(T[:200], sx2[:200], 'b')
+    plt.plot(T[:min_time_steps], sx1[:min_time_steps], 'r')
+    plt.plot(T[:min_time_steps], sx2[:min_time_steps], 'b')
     plt.ylabel('$<\sigma_x>$')
     plt.subplot(2,2,2)
     plt.ylim(top = 1)
     plt.ylim(bottom = -1)
-    plt.plot(T[:200], sy1[:200], 'r')
-    plt.plot(T[:200], sy2[:200], 'b')
+    plt.plot(T[:min_time_steps], sy1[:min_time_steps], 'r')
+    plt.plot(T[:min_time_steps], sy2[:min_time_steps], 'b')
     plt.ylabel('$<\sigma_y>$')
     plt.subplot(2,2,3)
     plt.ylim(top = 1)
     plt.ylim(bottom = -1)
-    plt.plot(T[:200], sz1[:200], 'r')
-    plt.plot(T[:200], sz2[:200], 'b')
+    plt.plot(T[:min_time_steps], sz1[:min_time_steps], 'r')
+    plt.plot(T[:min_time_steps], sz2[:min_time_steps], 'b')
     plt.xlabel('Time')
     plt.ylabel('$<\sigma_z>$')
     plt.subplot(2,2,4)
     plt.ylim(top = 0.6)
     plt.ylim(bottom = 0)
-    plt.plot(T[:200], tr_dist1[:200], 'r')
-    plt.plot(T[:200], tr_dist2[:200], 'b')
+    plt.plot(T[:min_time_steps], tr_dist1[:min_time_steps], 'r')
+    plt.plot(T[:min_time_steps], tr_dist2[:min_time_steps], 'b')
     plt.ylabel('Trace distance')
     plt.xlabel('Time')
     plt.savefig(f'pic/dynamics_fig{counter}.pdf')
