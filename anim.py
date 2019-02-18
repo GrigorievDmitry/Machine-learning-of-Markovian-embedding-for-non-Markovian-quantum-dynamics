477b6fc49da04b24f3701d1545c122caa812af7b
LuchnikovI <luchnikovilya@gmail.com>
2019-02-18 20:13:05 +0300

    Update anim.py

--- anim.py
+++ anim.py
@@ -1,7 +1,8 @@
 import imageio
-
+#This piece of code generate gif animation from set of pictures
 images = []
-filenames = [f'pic/dynamics_fig{i}.png' for i in range(300)]
+n = 300 #Number of pictures
+filenames = [f'pic/dynamics_fig{i}.png' for i in range(n)]
 for filename in filenames:
     images.append(imageio.imread(filename))
 imageio.mimsave('anim.gif', images)
