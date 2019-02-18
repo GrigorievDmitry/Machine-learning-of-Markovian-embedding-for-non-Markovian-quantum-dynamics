66c11b2b254d0791dcfacae6d33d6863a4ef38fa
LuchnikovI <luchnikovilya@gmail.com>
2019-02-18 20:05:40 +0300

    Update perturbated_hamiltonian.py

--- perturbated_hamiltonian.py
+++ perturbated_hamiltonian.py
@@ -3,7 +3,10 @@ import numpy.random as rnd
 import matplotlib.pyplot as plt
 import dynamics_learning as dl
 
-sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]])
+sigma = np.array([[[0., 1.],[1., 0.]], [[0., -1j],[1j, 0.]], [[1., 0.],[0., -1.]]])#The set of Pauli matrices
+
+
+#Initialization of two models and uploading predefined Hamiltonians
 model_1 = dl.dynamics_learning(2, 2, 2)
 model_2 = dl.dynamics_learning(2, 2, 4)
 model_1.set_in_state(np.array([[0., 0.], [0., 1.]]))
@@ -13,21 +16,34 @@ h_true = np.load('h_true.npy')
 model_1.set_h(h_true)
 model_2.set_h(h)
 
+
+#Pertubated hamiltonians
 h_pert = rnd.randn(2,2)
 h_pert = (h_pert + h_pert.conj().T)/2
 h_pert1 = 0.5*np.kron(h_pert, np.eye(4,4))
 h_pert2 = 0.5*np.kron(h_pert, np.eye(8,8))
 
-rho1 = model_1.get_evolution_h_pert_continuous(2000, 0.1, h_pert1)
-rho2 = model_2.get_evolution_h_pert_continuous(2000, 0.1, h_pert2)
+#==================================PARAMETERS===================================
+plotting_time_step = 0.1
+max_total_plotting_time = 200
+min_total_plotting_time = 20
+#===============================================================================
+max_time_steps = int(max_total_plotting_time/plotting_time_step)
+min_time_steps = int(min_total_plotting_time/plotting_time_step)
+
+#Dynamics simulation
+rho1 = model_1.get_evolution_h_pert_continuous(max_time_steps, plotting_time_step, h_pert1)
+rho2 = model_2.get_evolution_h_pert_continuous(max_time_steps, plotting_time_step, h_pert2)
 model_1.set_in_state(np.array([[1., 0.], [0., 1.]])/2)
 model_2.set_in_state(np.array([[1., 0.], [0., 1.]])/2)
-rho_zero1 = model_1.get_evolution_h_pert_continuous(2000, 0.1, h_pert1)
-rho_zero2 = model_2.get_evolution_h_pert_continuous(2000, 0.1, h_pert2)
+rho_zero1 = model_1.get_evolution_h_pert_continuous(max_time_steps, plotting_time_step, h_pert1)
+rho_zero2 = model_2.get_evolution_h_pert_continuous(max_time_steps, plotting_time_step, h_pert2)
 model_1.set_in_state(np.array([[0., 0.], [0., 1.]]))
 model_2.set_in_state(np.array([[0., 0.], [0., 1.]]))
-T = np.arange(0, 200, 0.1)
+T = np.arange(0, max_total_plotting_time, plotting_time_step)
 
+
+#Plotting
 sx1 = np.einsum('kij,ij->k', rho1, sigma[0])
 sx2 = np.einsum('kij,ij->k', rho2, sigma[0])
 sy1 = np.einsum('kij,ij->k', rho1, sigma[1])
@@ -72,25 +88,25 @@ fig2.subplots_adjust(wspace=0.25, hspace=0.35)
 plt.subplot(2,2,1)
 plt.ylim(top=1)
 plt.ylim(bottom=-1)
-plt.plot(T[:200], sx1[:200], 'r')
-plt.plot(T[:200], sx2[:200], 'b')
+plt.plot(T[:min_time_steps], sx1[:min_time_steps], 'r')
+plt.plot(T[:min_time_steps], sx2[:min_time_steps], 'b')
 plt.ylabel('$<\sigma_x>$')
 plt.subplot(2,2,2)
 plt.ylim(top=1)
 plt.ylim(bottom=-1)
-plt.plot(T[:200], sy1[:200], 'r')
-plt.plot(T[:200], sy2[:200], 'b')
+plt.plot(T[:min_time_steps], sy1[:min_time_steps], 'r')
+plt.plot(T[:min_time_steps], sy2[:min_time_steps], 'b')
 plt.ylabel('$<\sigma_y>$')
 plt.subplot(2,2,3)
 plt.ylim(top=1)
 plt.ylim(bottom=-1)
-plt.plot(T[:200], sz1[:200], 'r')
-plt.plot(T[:200], sz2[:200], 'b')
+plt.plot(T[:min_time_steps], sz1[:min_time_steps], 'r')
+plt.plot(T[:min_time_steps], sz2[:min_time_steps], 'b')
 plt.xlabel('Time')
 plt.ylabel('$<\sigma_z>$')
 plt.subplot(2,2,4)
-plt.plot(T[:200], tr_dist1[:200], 'r')
-plt.plot(T[:200], tr_dist2[:200], 'b')
+plt.plot(T[:min_time_steps], tr_dist1[:min_time_steps], 'r')
+plt.plot(T[:min_time_steps], tr_dist2[:min_time_steps], 'b')
 plt.ylabel('Trace distance')
 plt.xlabel('Time')
 plt.savefig(f'pic/perturbation.pdf')
